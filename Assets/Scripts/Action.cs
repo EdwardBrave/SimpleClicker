613a1fc574c79846f8593fe11a5880f899117876
EdwardBrave <edward.brave45@gmail.com>
2019-10-14 22:49:35 +0300

    init commit

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour, IStrategy
+{
+    public void action()
+    {
+        Debug.Log("Hello my first strategy");
+    }
+}
